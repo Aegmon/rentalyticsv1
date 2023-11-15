5a90c4d8aa9d080a4eb9c3808c565a95bc05692e
Aegmon <pinedabennor@gmail.com>
2023-11-15 15:24:04 +0800

    newvendor

---
+++ vendor1/vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php
+
+return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'stripe/stripe-php' => array(
+            'pretty_version' => 'v12.6.0',
+            'version' => '12.6.0.0',
+            'reference' => 'e0c15e4cbf252e708b937482bb1a50fa7e01bc9d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stripe/stripe-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
